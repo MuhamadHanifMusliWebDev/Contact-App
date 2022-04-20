0699907a017c7453b2a3a60088deeca75e395e10
MuhamadHanifMusliWebDev <103912913+MuhamadHanifMusliWebDev@users.noreply.github.com>
2022-04-20 11:05:02 +0700

    Add files via upload

--- Contact App/app.js
+++ Contact App/app.js
@@ -1,23 +1,28 @@
-const {question, saveContact} = require('./contact')
+const yargs = require('yargs');
+const contact = require('./contact')
 
-const allQuestions = async () => {
-  const name = await question('Enter Your Name: ')
-  const email = await question('Enter Your Email: ')
-  const noPhone = await question('Enter Your Mobile Phone Number: ')
-  
-  saveContact(name, email, noPhone)
-}
-allQuestions()
-
-// rl.question('Input Your Name: ', name => {
-//   rl.question('Input Your Mobile Phone Number: ', noMobile => {
-//     const contact = {name, noMobile}
-//     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8')
-//     const contacts = JSON.parse(fileBuffer)
-
-//     contacts.push(contact)
-//     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
-//     console.log(`Thank You For Filling In The Data ${name}!`)
-//     rl.close()
-//   })
-// })
+yargs.command({
+  command: 'add',
+  describe: 'Adding new contact',
+  builder: {
+    name: {
+      describe: 'Your full name',
+      demandOption: true,
+      type: 'string'
+    },
+    email: {
+      describe: 'Enter your email address',
+      demandOption: false,
+      type: 'string'
+    },
+    phoneNumber: {
+      describe: 'Enter your phone number',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    contact.saveContact(argv.name, argv.email, argv.phoneNumber)
+  }
+})
+yargs.parse()
--- Contact App/contact.js
+++ Contact App/contact.js
@@ -1,18 +1,20 @@
 const fs = require('fs')
-const readline = require('readline')
+const chalk = require('chalk')
+const validator = require('validator')
+// const readline = require('readline')
 
-const rl = readline.createInterface({
-  input : process.stdin,
-  output : process.stdout
-})
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
 
 const dirPath = './data';
-if (!fs.existsSync(dirPath)){
+if (!fs.existsSync(dirPath)) {
   fs.mkdirSync(dirPath)
 }
 
 const dataPath = './data/contacts.json';
-if (!fs.existsSync(dataPath)){
+if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
@@ -40,26 +42,48 @@ if (!fs.existsSync(dataPath)){
 //   })
 // }
 
-const question = (question) => {
-  return new Promise((resolve, reject) => {
-    rl.question(question, answer => {
-      resolve(answer)
-    })
-  })
-}
+// const question = (question) => {
+//   return new Promise((resolve, reject) => {
+//     rl.question(question, answer => {
+//       resolve(answer)
+//     })
+//   })
+// }
 
-const saveContact = (name, email, noPhone) => {
-  const contact = {name, email, noPhone}
+const saveContact = (name, email, phoneNumber) => {
+  const contact = {
+    name,
+    email,
+    phoneNumber
+  }
   const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8')
   const contacts = JSON.parse(fileBuffer)
 
+  const duplicates = contacts.find((contacts) => contacts.name === name)
+  if (duplicates) {
+    console.log(chalk.red.inverse.bold('Contact is registered! Use another name'))
+    return false
+  }
+
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold('Email is not valid! Enter your correct email!'))
+      return false
+    }
+  }
+
+  if (!validator.isMobilePhone(phoneNumber, 'id-ID')) {
+    console.log(chalk.red.inverse.bold('Phone number is not valid! Enter your correct phone number!'))
+    return false
+  }
+
   contacts.push(contact)
   fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
-  console.log(`Thank You For Filling In The Data!`)
-  rl.close()
+  console.log(chalk.green.inverse.bold(`Thank You For Filling In The Data!`))
+  // rl.close()
 }
 
 module.exports = {
-  question,
+  // question,
   saveContact
 }
